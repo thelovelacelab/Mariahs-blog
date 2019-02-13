642a19123673eac568af427a18f810f4dc05fdf0
thelovelacelab <35945953+thelovelacelab@users.noreply.github.com>
2019-02-12 18:23:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+/* DROP DOWN MENU */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+
+// PUSH SIDE MENU
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+
+/* MORE  
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+*/
+
+function myFunction(dots, more, myBtn) {
+  var dots = document.getElementById(dots);
+  var moreText = document.getElementById(more);
+  var btnText = document.getElementById(myBtn);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+
+
+
+  
+
+
+/*
+// SLIDE SHOW
+var slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+ 
+
+  var dots = document.getElementsByClassName("dot");
+  
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 3 seconds
+}
+*/
+
+// SLIDE SHOW
+function showSlides(className, slideStart) {
+   var i;
+   var myIndex = slideStart;
+   var x = document.getElementsByClassName(className);
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}
+
+      x[myIndex-1].style.display = "block";
+
+      setTimeout(showSlides, 2000, className, myIndex);
+}
+
+showSlides("mySlides", 0);
+showSlides("mySlides2", 3);
+showSlides("mySlides3", 6);
+showSlides("mySlides4", 9);
+
+
+
+
+
+
+
+
+
+
+
